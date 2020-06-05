c70c529ed41fc468488b92538d1792c610618c95
duzhanneng <903882310@qq.com>
2020-06-05 19:19:00 +0800

    增加初始化initEvent

--- examples/game-jump-client-demo/src/main.js
+++ examples/game-jump-client-demo/src/main.js
@@ -2,6 +2,12 @@ import JumpGameWorld from './jump/index';
 import { logFps, getIsHyExt, getVersion } from './jump/utils';
 // import Stats from 'stats.js';
 
+if (!window.Event.initEvent) {
+  window.Event.initEvent = function (name) {
+    this._target = name;
+  };
+}
+
 const createCanvas = document.createElement('canvas');
 createCanvas.id = 'jump-world-canvas';
 document.body.appendChild(createCanvas);
@@ -14,7 +20,7 @@ document.body.appendChild(createCanvas);
     canvas: createCanvas,
     axesHelper: false, // 辅助线
   });
-  
+
   const version = await getVersion();
   if (!isHyExt || ~[1, 2].indexOf(~~version)) {
     logFps();

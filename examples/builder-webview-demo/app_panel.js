0a4861d47e1e4b4b11c5ad6810402376c1f64d86
Simon Lu <luzuoqing@huya.com>
2023-04-20 17:48:58 +0800

    feat: add builder-webview-dmeo

---
+++ examples/builder-webview-demo/app_panel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import GameView from '@hyext/gameview'
+
+const App = () => {
+  return <GameView />
+}
+
+AppRegistry.registerComponent('app', () => App)
---
+++ examples/builder-webview-demo/my-game-files/hyext-outline-5fb1b8.js
@@ -0,0 +1,12 @@
+
+    if (typeof window.process === 'undefined') {
+      window.process = {}
+    }
+    if (typeof window.process.env === 'undefined') {
+      window.process.env = {}
+    }
+
+    window.process.env.HYEXT_BUILD_ENV = 'dev'
+    window.process.env.HYEXT_EXT_UUID = '0'
+    window.process.env.HYEXT_BUILD_EXT_TYPE = 'pc' === 'app' ? 'rn' : 'h5'
+  
